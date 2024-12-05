44686ba5df890a860eb130b08b8ecbb1ef8ee28f
Cubalalah <yannou.chabance@gmail.com>
2024-12-05 15:34:15 +0100

    resolution of 05

--- 05.cpp
+++ 05.cpp
@@ -21,37 +21,62 @@ vector<int> vectorize(string line) {
         comma_pos = line.find(",", initialPos);
     }
 
+    line_vector.push_back(stoi(line.substr(initialPos, line_vector.size() - initialPos)));
+
     return line_vector;
 }
 
-bool isCorrect(string line) {
-    vector<int> line_vector = vectorize(line);
+bool isCorrect(vector<int> line_vector, vector<tuple<int, int>> rules) {
+    for (int i = 1; i < line_vector.size(); i++) {
+        if (find(rules.begin(), rules.end(), make_tuple(line_vector[i-1], line_vector[i])) == rules.end()) {
+            return false;
+        }
+    }
+    return true;
+}
 
-   
+vector<int> becomeCorrect(vector<int> line_vector, vector<tuple<int, int>> rules) {
+    int i = 1;
+    while (!isCorrect(line_vector, rules)) {
+        if (find(rules.begin(), rules.end(), make_tuple(line_vector[i - 1], line_vector[i])) == rules.end()) {
+            auto temp = line_vector[i];
+            line_vector[i] = line_vector[i - 1];
+            line_vector[i - 1] = temp;
+            i = 1;
+        }
+        else {
+            i++;
+        }
+    }
+    return line_vector;
 }
 
 int main() {
     ifstream input("05.txt");
     string line;
     vector<tuple<int,int>> rules;
-    set<int> all_pages;
 
     while (getline(input, line) && line[0] != '\0') {
         int a = stoi(line.substr(0, line.find("|")));
         int b = stoi(line.substr(line.find("|")+1, line.size() - line.find("|")-1));
         rules.push_back(make_tuple(a, b));
-        all_pages.insert(a);
-        all_pages.insert(b);
     }
 
-    int correct_updates_counter = 0;
+    int part1 = 0;
+    int part2 = 0;
 
     while (getline(input, line)) {
-        if (isCorrect(line)) {
-            correct_updates_counter++;
+        vector<int> line_vector = vectorize(line);
+        if (isCorrect(line_vector, rules)) {
+            part1 += line_vector[line_vector.size()/2];
+        }
+        else {
+            vector<int> new_vector = becomeCorrect(line_vector, rules);
+            part2 += new_vector[new_vector.size() / 2];
         }
     }
 
-    cout << "Part 1 : Number of updates in the correct order : " << correct_updates_counter;
+    cout << "Part 1 : Sum of middle page numbers of updates in the correct order : " << part1 << "\n";
+    cout << "Part 2 : Sum of middle page numbers of updates in the incorrect order : " << part2;
     return 0;
 }
